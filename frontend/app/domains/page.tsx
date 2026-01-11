5a67ce483c45cfcfeaeb46cb9b1082314e47428f
Dreadnought <joe@darkspacelabs.com>
2026-01-10 23:55:05 -0700

    Update placeholder text for zone_id input field

--- frontend/app/domains/page.tsx
+++ frontend/app/domains/page.tsx
@@ -169,7 +169,7 @@ export default function DomainsPage() {
                           id="zone_id"
                           required
                           className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm font-mono"
-                          placeholder="d1b0be3b21c3dae226bb0f1d57a66f7c"
+                          placeholder="3BJnnfSHADoMqYgvaRx8V7D8p5fcRGyQ"
                           maxLength={32}
                           value={formData.zone_id}
                           onChange={(e) => setFormData({ ...formData, zone_id: e.target.value.toLowerCase() })}
